743f09e468e21dae0d32d1a67477a052adbaeea6
shashvat <laneteam.shashvat@gmail.com>
2018-01-23 18:20:45 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,16 +14,23 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
 
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt:new Date().getTime()
+
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: `Hello ${message.from}! Welcome to the chat app`, 
+            createdAt: new Date().getTime()
+        });
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: `New user joined ${message.from}`,
+            createdAt: new Date().getTime()
         });
     });
 
-   
+
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
