ca76f738cc456274a795feff110d2db9242aa575
shashvat <laneteam.shashvat@gmail.com>
2018-01-24 12:35:10 +0530

    Add geoLocation support via geoLocation api

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,15 @@ socket.on('newMessage', function (message) {
     $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = $('<li></li>');
+    var a = $('<a target="_blank">My current location</a>');
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+    $('#messages').append(li);
+});
+
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
     var text = $('[name=message]').val()
@@ -28,4 +37,21 @@ $('#message-form').on('submit', function (e) {
         from: 'Swapnil',
         text
     }, function () { });
+});
+
+var locationButton = $("#send-location");
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position);
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location');
+    });
 });
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const { generateMessage } = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -13,7 +13,7 @@ app.use(express.static(publicPath));
 var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected');
-    socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', `New user joined`));
 
 
@@ -21,6 +21,10 @@ io.on('connection', (socket) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback();
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
 
@@ -31,5 +35,5 @@ io.on('connection', (socket) => {
 })
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+    console.log(`Server is up on port ${ port }`);
 });
